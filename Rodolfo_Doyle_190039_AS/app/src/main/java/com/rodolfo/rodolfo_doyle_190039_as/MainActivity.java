8e03480258c2cd66a0eec1cc19fefd5930fee239
RodolfoDoyle <rodolfo.doyle@gmail.com>
2021-06-24 19:53:54 -0300

    Questão5
    Chama na pergunta 5

--- Rodolfo_Doyle_190039_AS/app/src/main/java/com/rodolfo/rodolfo_doyle_190039_as/MainActivity.java
+++ Rodolfo_Doyle_190039_AS/app/src/main/java/com/rodolfo/rodolfo_doyle_190039_as/MainActivity.java
@@ -9,24 +9,22 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+//Variavel
     FloatingActionButton fab = findViewById(R.id.fab);
     @Override
-
+//Criar instancias
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
+//Layout
         setContentView(R.layout.activity_main);
-
+//O que o botão faz qdo clica
         fab.setOnClickListener(new View.OnClickListener() {
-
             @Override
-
+//Configurando o que rola quando clica
             public void onClick(View view) {
-
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-
+//Texto que aparece quando clica
+                Snackbar.make(view, "DALE OHATÃO", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+//Tamo junto
             }
         });
     }
